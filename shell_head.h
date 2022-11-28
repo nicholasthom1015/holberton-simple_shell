454f17729693b1b8062381f80c169e0a7131191a
Oggal <Philip.dan.taylor@gmail.com>
2022-11-28 10:19:19 -0800

    Add Header
    Contains Includes from practice script

---
+++ shell_head.h
@@ -0,0 +1,20 @@
+#ifndef MY_SHELL_HEAD
+#define MY_SHELL_HEAD
+/* INCLUDES*/
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* STRUCTURES */
+
+
+
+/* FUNCTIONS*/
+
+
+#endif
