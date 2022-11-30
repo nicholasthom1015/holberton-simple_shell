139ba373299887dbb2b850b4e039b87bf25d37f1
nicholasthom1015 <nicholasthomas10.15@gmail.comas10.15@gmail.com>
2022-11-30 12:00:54 -0800

    Ops.c update

--- ops.c
+++ ops.c
@@ -5,14 +5,13 @@
 int Run_Command(char **argV)
 {
 	pid_t childPID;
-	int zero = 0;
 	/* if passed NULL, Return -1 */
 	if (argV == NULL || argV[0] == NULL)
 		return (-1);
 	childPID = fork();
 	if(childPID == 0)
 	{
-		if (execve(argV[0], argV, NULL) == -1);
+		if (execve(argV[0], argV, NULL) == -1)
 		exit(errno);
 	}
 	else
