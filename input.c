438355d5809b97ff9875b28478f4c3f8775455ea
nicholasthom1015 <nicholasthomas10.15@gmail.comas10.15@gmail.com>
2022-11-30 12:12:03 -0800

    Betty checked Input.c

--- input.c
+++ input.c
@@ -1,5 +1,12 @@
 #include "shell_head.h"
 
+/**
+ * **Prep_Input - prepares input for shell
+ * @line: length of string
+ * @resultArray: buffer to store the result
+ * Return: result of array
+ */
+
 char **Prep_Input(char *line, char **resultArray)
 {
 	char *curToken;
@@ -7,7 +14,7 @@ char **Prep_Input(char *line, char **resultArray)
 	int i;
 
 
-	if(resultArray == NULL)
+	if (resultArray == NULL)
 		resultArray = malloc(16 * sizeof(char *));
 
 	if (resultArray == NULL)
@@ -15,19 +22,25 @@ char **Prep_Input(char *line, char **resultArray)
 		return (NULL);
 	}
 	curToken = strtok(line, delim);
-        for (i = 0; curToken && i < 15; i++)
+	for (i = 0; curToken && i < 15; i++)
 	{
 		resultArray[i] = curToken;
 		curToken = strtok(NULL, delim);
 	}
 	resultArray[i] = NULL;
-        return(resultArray);
+	return (resultArray);
 }
+/**
+ * Validate_Input - validates input for shell
+ * @tokens: tokens
+ * Return: result
+ */
 
 int Validate_Input(char **tokens)
 {
 	struct stat sb;
 	int result;
+
 	result = (stat(tokens[0], &sb) == 0);
 	return (result);
 }
